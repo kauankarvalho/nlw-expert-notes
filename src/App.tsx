ed6678bc469c3f7a5e07004e13132a182deb9e51
kauankarvalho <kauan.karvalho@outlook.com>
2024-02-20 11:57:38 -0300

    feat: add header to the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import logo from "./assets/logo-nlw-expert.svg"
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="mx-auto my-12 max-w-6xl space-y-6">
+      <header className="space-y-6">
+        <img src={logo} alt="NLW Expert" />
+
+        <form className="w-full">
+          <input
+            type="text"
+            placeholder="Busque em suas notas..."
+            className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          />
+        </form>
+
+        <div className="h-px w-full bg-slate-700"></div>
+      </header>
+    </div>
+  )
 }
